12e481576fffaf588c9f0f3f4a4dd30be43bef8e
FlorianiL <181847@umons.ac.ba>
2021-04-07 21:41:54 +0200

    fix error with '}}'

--- src/lark_parser.py
+++ src/lark_parser.py
@@ -1,55 +1,48 @@
-from lark import Lark, Transformer, v_args
+from lark import Lark
 
 calc_grammar = """
     start : program
     
-    program : program_t | program_b | program_tp | program_bp
+    program : txt
+            | txt program
+            | dumbo_block
+            | dumbo_block program
     
-    program_t : txt
+    txt : /[ a-zA-Z0-9_;&<>"-.:,\n]+/x
     
-    program_b : dumbo_bloc
+    dumbo_block : ("{{" "}}")
+                | ("{{" expression_list "}}")
     
-    program_tp : txt program
-    
-    program_bp : dumbo_bloc program
-    
-    txt : expression_list_rec
-        | expression
-    
-    dumbo_bloc : "{{" expression_list_rec "}}"
-    
-    expression_list_rec : expression ";" expression_list_rec
-    
-    expression_list_end : expression ";"
-    
+    expression_list : (expression ";" expression_list)
+                    | (expression ";")
+        
     expression : expression_print
-               | expression_for_list
+               | expression_for_lis
                | expression_for_var 
                | expression_var
     
     expression_print : "print" string_expression
     
-    expression_for_list : "for" VARIABLE "in" string_list "do" expression_list_rec "endfor"
+    expression_for_lis : "for" VARIABLE "in" string_list "do" expression_list "endfor"
     
-    expression_for_var :  "for" VARIABLE "in" VARIABLE "do" expression_list_rec "endfor"
+    expression_for_var :  "for" VARIABLE "in" VARIABLE "do" expression_list "endfor"
     
-    expression_var : VARIABLE ":=" string_expression
-                   | VARIABLE ":=" string_list 
+    expression_var : (VARIABLE ":=" string_expression)
+                   | (VARIABLE ":=" string_list)
                 
-    string_expression : string
+    string_expression : STRING
                       | VARIABLE
-                      | string_expression "." string_expression
+                      | (string_expression "." string_expression)
                           
     string_list : "(" string_list_interior ")"
     
-    string_list_interior : string
-                         | string "," string_list_interior
-    
-    string : "'"/\w+/"'"
-                             
+    string_list_interior : STRING
+                         | (STRING "," string_list_interior)
+                                 
     VARIABLE : /[a-zA-Z_]\w*/
-            
-    %ignore /[ \t\f\r]+/
+    STRING : /'[^']+'/
+    
+    %ignore /[ \t\f\r\n]+/x
 
 """
 calc_parser = Lark(calc_grammar, parser='lalr')
