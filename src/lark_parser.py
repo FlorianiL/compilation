186b7c225b4d2f806deb3c2b222e113eb6bf130e
FlorianiL <181847@umons.ac.ba>
2021-04-09 20:48:30 +0200

    methods for string list works

--- src/lark_parser.py
+++ src/lark_parser.py
@@ -1,3 +1,5 @@
+import copy
+
 import lark.tree
 from lark import Lark
 from lark import Transformer
@@ -24,17 +26,17 @@ calc_grammar = """
                | expression_for_var 
 
     
-    expression_var : (NAME_VAR ":=" string_expression)
-                   | (NAME_VAR ":=" string_list)
+    expression_var : (variable_set ":=" string_expression)
+                   | (variable_set ":=" string_list)
     
     expression_print : "print" string_expression
     
-    expression_for_lis : "for" NAME_VAR "in" string_list "do" expression_list "endfor"
+    expression_for_lis : "for" variable_set "in" string_list "do" expression_list "endfor"
     
-    expression_for_var :  "for" NAME_VAR "in" var "do" expression_list "endfor"
+    expression_for_var :  "for" variable_set "in" variable_get "do" expression_list "endfor"
                 
     string_expression : string
-                      | var
+                      | variable_get
                       | string_concat
 
     string_concat : string_expression "." string_expression
@@ -44,8 +46,8 @@ calc_grammar = """
     string_list_interior : string
                          | (string "," string_list_interior)
     
-    var : /[a-zA-Z_]\w*/   
-    NAME_VAR : /[a-zA-Z_]\w*/
+    variable_set : /[a-zA-Z_]\w*/   
+    variable_get : /[a-zA-Z_]\w*/
     string : /'[^']+'/
     
     %ignore /[ \t\f\r\n]+/x
@@ -56,87 +58,91 @@ calc = calc_parser.parse
 
 
 class TreeToDumbo(Transformer):
+
     def __init__(self, visit_tokens=True):
         super().__init__(visit_tokens)
         self.vars = {}
-
-    def transform(self, tree):
-        super().transform(tree)
-        print(self.vars)
+        self.temp = []
 
     def program(self, items):
-        print(items)
-
-    def txt(self, items):
-        print(items)
+        print(f"program --> {items}")
+        return items
 
     def dumbo_block(self, items):
-        print(items)
+        print(f"dumbo_block --> {items}")
+        return items
 
     def expression_list(self, items):
         print(f"expression_list --> {items}")
+        return items
 
     def expression(self, items):
         print(f"expression --> {items}")
+        return items
 
-    def expression_var(self, items):
+    def expression_var(self, items): # OK
         name, value = items
-        if type(value) == lark.tree.Tree:
-            value = self.transform(value)
         self.vars[name] = value
-        return value
+        print(f"expression_var --> {name} {self.vars[name]}")
 
     def expression_print(self, items):
-        (value,) = items
-        if type(value) == lark.tree.Tree:
-            value = self.transform(value)
-        print(f"print --> {value}")
-        return print(value)
+        print(f"expression_print --> {items}")
+        return items
 
     def expression_for_lis(self, items):
-        print(f"for_lis --> {items}")
+        print(f"expression_for_lis --> {items}")
+        return items
 
     def expression_for_var(self, items):
-        print(f"for_var --> {items}")
+        print(f"expression_for_var --> {items}")
+        return items
 
-    def string_expression(self, items):
-        print(items)
+    def string_expression(self, items): # OK
+        (res,) = items
+        print(f"string_expression --> {res}")
+        return res
 
-    def string_concat(self, items):
+    def string_concat(self, items): # NOT TEST !!!
+        print(f"string_concat --> {items}")
         (str1,), (str2,) = items
         return str1[1:-1] + str2[1:-1]
 
-    def string_list(self, items):
-        print(f"string_list ---> {items}")
-        (val,) = items
-        print(val)
-        for item in items:
-            print(type(item))
-        return list(self.transform(items))
+    def string_list(self, items): # OK
+        res = self.temp
+        self.temp = []
+        print(f"string_list ---> {res}")
+        return res
 
-    def string_list_interior(self, items):
-        print(f"string_list_interior ---> {items}")
-        if len(items) > 1:
-            value, rest = items
-            print(rest)
-        else:
-            value = items
-        print(f"string_list_interior ---> {value}")
-        return value
+    def string_list_interior(self, items): # OK
+        self.temp.append(items[0])
+        print(f"string_list_interior ---> {self.temp} ")
 
-    def var(self, item):
+    def variable_get(self, item):
         (name,) = item
-        print(f"name ---> {name}")
+        print(f"variable_get ---> {name}")
         return name
 
-    def string(self, item):
+    def variable_set(self, item):
+        (name,) = item
+        print(f"variable_set ---> {name}")
+        return name
+
+    def string(self, item):     # OK
         (s,) = item
-        return s[1:-1]
+        res = s[1:-1]
+        print(f"string --> {res}")
+        return res
+
+    def txt(self, item):        # OK
+        (res,) = item
+        print(f"txt --> {res}")
+        return res
+
 
 def test_lark(data, template):
     tree_data = calc(data)
     tree_template = calc(template)
     print("************************ DATA **********************************")
     TreeToDumbo().transform(tree_data)
-    print("************************ TEMPLATE **********************************")
-    TreeToDumbo().transform(tree_template)
+    # print("************************ TEMPLATE **********************************")
+    # TreeToDumbo().transform(tree_template)
